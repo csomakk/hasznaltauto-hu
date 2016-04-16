9c114ec495ef8c1d6443747a2afd15bf340850f4
Gabor Csomak <csomakk@gmail.com>
2016-04-16 22:01:42 +0100

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -34,7 +34,7 @@ do {
       scraperwiki::save(   
         array('id'),
         array(
-          'id' => $kod + "-" date("Y/m/d")
+          'id' => $kod + "-" + date("Y/m/d"),
           'kod' => $kod,
           'type' => $el->innertext,
           'price' => $ar,
