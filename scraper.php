f41bba517c105779ea6ef32a93bbc5cd0ea08fa7
Gabor Csomak <csomakk@gmail.com>
2016-04-16 21:45:57 +0100

    indents

--- scraper.php
+++ scraper.php
@@ -5,50 +5,45 @@ $page_counter = 0;
 $next_page = FALSE;
 
 do { 
-$kovetkezo = "";
-$page_counter++;
-$pageurl = "http://www.hasznaltauto.hu/talalatilista/auto/0P5EDPTSFJF2GPQWMZ8FE4PFQ0AEPWW8WTD7U5FKC38K84H2R8G1OR2Z102S3SSE9LJG2ORW356KS2I9QPOG3CPYEOYROPKTWW8H9J8H3OIWP86DTCG02PYHLFM9M7DWJQWE7QWY3M6HADCSKLY5CUITACLM537UKTJUAE1CYZE3WRISDY6Z0UCA7D6AHGWKF6C6RG/page{$page_counter}";
-
- 
-$html_content = scraperWiki::scrape($pageurl);
- 
-
-$html = str_get_html($html_content);
-foreach ($html->find("div.talalati_lista") as $talalat) {  
-    foreach ($talalat->find("h2 a") as $el) {
-    $tipus = $el->innertext;
-    $url = $el->href;
-    $kod = substr($url, -7); 
-    }
-    foreach ($talalat->find("div.talalati_lista_vetelar strong") as $ar) {
-    $ar = str_replace("&nbsp;", " ", $ar->innertext);
-    }
-    foreach ($talalat->find("p.talalati_lista_infosor") as $info) {
-
-    $info = str_replace("&ndash;", ",", $info->innertext);
-    $info = str_replace("&nbsp;", " ", $info);
-    $info = str_replace("&sup3;", "3", $info);
-    $info = explode(",",$info);
-    }
-
-    scraperwiki::save(   
-            array('id'),
-            array(
-                'id' => $kod,
-                'type' => $el->innertext,
-                'price' => $ar,
-                'year' => $info[0],
-                'benzin' => $info[1],
-                'hub' => $info[2],
-                'power' => $info[3],
-                'url' => $url,
-
-            )
-        );
-}
-    foreach ($html->find("div.oldalszamozas a[title=Következő oldal]") as $kovetkezo) {
-    print $page_counter . "\n";
-    }
-
+   $kovetkezo = "";
+   $page_counter++;
+   $pageurl = "http://www.hasznaltauto.hu/talalatilista/auto/0P5EDPTSFJF2GPQWMZ8FE4PFQ0AEPWW8WTD7U5FKC38K84H2R8G1OR2Z102S3SSE9LJG2ORW356KS2I9QPOG3CPYEOYROPKTWW8H9J8H3OIWP86DTCG02PYHLFM9M7DWJQWE7QWY3M6HADCSKLY5CUITACLM537UKTJUAE1CYZE3WRISDY6Z0UCA7D6AHGWKF6C6RG/page{$page_counter}";
+   
+   $html_content = scraperWiki::scrape($pageurl);
+    
+   $html = str_get_html($html_content);
+   foreach ($html->find("div.talalati_lista") as $talalat) {  
+      foreach ($talalat->find("h2 a") as $el) {
+        $tipus = $el->innertext;
+        $url = $el->href;
+        $kod = substr($url, -7); 
+      }
+      foreach ($talalat->find("div.talalati_lista_vetelar strong") as $ar) {
+        $ar = str_replace("&nbsp;", " ", $ar->innertext);
+      }
+      foreach ($talalat->find("p.talalati_lista_infosor") as $info) {
+        $info = str_replace("&ndash;", ",", $info->innertext);
+        $info = str_replace("&nbsp;", " ", $info);
+        $info = str_replace("&sup3;", "3", $info);
+        $info = explode(",",$info);
+      }
+   
+      scraperwiki::save(   
+        array('id'),
+        array(
+          'id' => $kod,
+          'type' => $el->innertext,
+          'price' => $ar,
+          'year' => $info[0],
+          'benzin' => $info[1],
+          'hub' => $info[2],
+          'power' => $info[3],
+          'url' => $url,
+        )
+      );
+   }
+   foreach ($html->find("div.oldalszamozas a[title=Következő oldal]") as $kovetkezo) {
+     print $page_counter . "\n";
+   }
 } while ($kovetkezo != "");
 ?>
